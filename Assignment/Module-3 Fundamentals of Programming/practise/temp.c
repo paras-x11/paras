626478720c645ba8d8cd730464e480ed90063ce1
paras <ranaparas9375@gmail.com>
2024-06-12 21:46:13 +0530

    add

--- Assignment/Module-3 Fundamentals of Programming/practise/temp.c
+++ Assignment/Module-3 Fundamentals of Programming/practise/temp.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
-void main(){
-    for (int r = 1; r <= 5; r++) {
+// #include<stdio.h>
+// void main(){
+//     for (int r = 1; r <= 5; r++) {
 
-        for (int c = 1; c <= 5; c++) {
+//         for (int c = 1; c <= 5; c++) {
 
-            if  (r == 3 &&  c == 2) {
-                printf("- ");  
-            } 
+//             if  (r == 3 &&  c == 2) {
+//                 printf("- ");  
+//             } 
             
-            else {
-                printf("* ");
-            }
-        }
-        printf("\n");
-    } 
-}
+//             else {
+//                 printf("* ");
+//             }
+//         }
+//         printf("\n");
+//     } 
+// }
 
 
 // if (i == 1 && j == 2) {
@@ -23,3 +23,35 @@ void main(){
 // if (i == 1 && j == 2) {
 // Character at row 2, column 3:
 // }
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int p;
+    char ch;
+
+    start:
+    printf("\nEmter value: ");
+    scanf("%d",&p);
+
+    printf("\np * 2 = %d",p*2);
+
+    while ((getchar()) != '\n'); // This consumes all characters in the input buffer until a newline
+
+
+    printf("\nDo You Want To Place More Order: y or n: ");
+    scanf(" %c", &ch);
+
+    if(ch == 'y' || ch == 'Y'){
+
+        goto start;
+    
+    }
+    
+    else{
+        printf("\nexited.");
+    }
+    return 0;
+}
