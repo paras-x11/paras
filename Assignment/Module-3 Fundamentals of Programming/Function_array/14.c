8d2a3ebfbe02cbd696d3ab2e2255daea0c9e04f6
paras <ranaparas9375@gmail.com>
2024-06-22 10:56:38 +0530

    add

--- Assignment/Module-3 Fundamentals of Programming/Function_array/14.c
+++ Assignment/Module-3 Fundamentals of Programming/Function_array/14.c
@@ -1,128 +1,136 @@
 // 14. Perform 2D matrix array 
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-int row,col, element;
 
-void search(int arr[][col],int row, int col, int element){
-    int r, c, found=0;
+int r, c, row, col, element;
+
+void search(int arr[][col], int row, int col, int element){
+    int flag=0;
+    
     for(r=0; r<row; r++){
         for(c=0; c<col; c++){
             if(arr[r][c] == element){
-               printf("-> Element %d found at position: [%d][%d]\n",element,r,c);
-               found=1;
+                printf("\n-> %d is found at position [%d][%d]\n",element, r, c);
+                flag = 1;
             }
         }
-    } 
-    if (!found) {
-        printf("\n-> Element %d not found in the array.\n", element);
+    }
+    if(!flag){
+        printf("\n-> %d is not found in array.\n",element);
     }
 }
-
 int main(){
-    int r, c, ch;
-
-    printf("\nEnter row: ");
-    scanf("%d", &row);
-
-    printf("\nEnter column: ");
-    scanf("%d", &col);
-
-    int arr1[row][col], arr2[row][col], sum[row][col], sub[row][col], mul[row][col];
-
-    printf("\nEnter elemnets for array 1: ");               // enter array 1.
+    int ch;
+    
+    printf("\nenter row: ");
+    scanf("%d",&row);
+    printf("\nenter column: ");
+    scanf("%d",&col);
+    
+    int arr1[row][col], arr2[row][col], mul[row][col];
+    
+    printf("\nenter element for array 1: ");                // enter array 1
     for(r=0; r<row; r++){
         for(c=0; c<col; c++){
-            printf("\nEnter [%d][%d]: ",r, c);
             scanf("%d",&arr1[r][c]);
         }
     }
     
-    printf("\nEnter elemnets for array 2: ");               // enter array 2.
+    printf("\nenter element for array 2: ");                // enter array 2
     for(r=0; r<row; r++){
         for(c=0; c<col; c++){
-            printf("\nEnter [%d][%d]: ",r, c);
             scanf("%d",&arr2[r][c]);
         }
     }
-
-    printf("\nArray 1: \n");                // display array 1.
+    
+    printf("\narray 1: \n");                        // display array 1
     for(r=0; r<row; r++){
         for(c=0; c<col; c++){
-            printf("%d\t", arr1[r][c]);
+            printf("%d\t",arr1[r][c]);
         }printf("\n");
     }
     
-    printf("\nArray 2: \n");                // display array 2.
+    printf("\narray 2: \n");                        // display array 2
     for(r=0; r<row; r++){
         for(c=0; c<col; c++){
-            printf("%d\t", arr2[r][c]);
+            printf("%d\t",arr2[r][c]);
         }printf("\n");
     }
     
     while(1){
-    printf("\n-----------------------------------------");
-    printf("\nEnter 1 for search element in array 1.");
-    printf("\nEnter 2 for search element in array 2.");
-    printf("\nEnter 3 for sum.");
-    printf("\nEnter 4 for subtraction.");
-    printf("\nEnter 5 for multiplication.");
-    printf("\nEnter 0 for exit.");
-    printf("\n-----------------------------------------\n");
-    
-    printf("\nEnter your choice: ");
+    printf("\n1 for search in array 1.\n2 for search in array 2.\n3 for sum.\n");
+    printf("4 for substraction\n5 for multiplication\n6 for transpose both array.\n0 for exit.\n");
+    printf("\nenter your choice: ");
     scanf("%d",&ch);
     
     switch(ch){
-        case 1: printf("\nEnter Element to search in array 1: ");
-                scanf("%d",&element);
-                
-                search(arr1, row, col, element); 
-                break;
-    
-        case 2: printf("\nEnter Element to search in array 2: ");
-                scanf("%d",&element);
-                
-                search(arr2, row, col, element); 
-                break;
-                
-        case 3: printf("\nSum is: \n");
-                for(r = 0; r < row; r++) {
-                    for(c = 0; c < col; c++) {
-                        sum[r][c] = arr1[r][c] + arr2[r][c];
-                        printf("%d\t", sum[r][c]);
-                    }
-                    printf("\n");
-                }
-                break;
+        case 1:
+            printf("\nEnter element to search in array 1: ");
+            scanf("%d",&element);
+            search(arr1, row, col, element);
+        break;
+        
+        case 2:
+            printf("\nEnter element to search in array 2: ");
+            scanf("%d",&element);
+            search(arr2, row, col, element);
+        break;
             
-        case 4: printf("\nSubtraction is: \n");
-                for(r = 0; r < row; r++) {
-                    for(c = 0; c < col; c++) {
-                        sub[r][c] = arr1[r][c] - arr2[r][c];
-                        printf("%d\t", sub[r][c]);
-                    }
-                    printf("\n");
-                }
-                break;
-                
-        case 5: printf("\nmultiplication is: \n");
-                for(r = 0; r < row; r++) {
-                    for(c = 0; c < col; c++) {
+        case 3:
+            printf("\nSum is: \n");
+            for(r=0; r<row; r++){
+                for(c=0; c<col; c++){
+                    printf("%d\t",arr1[r][c] + arr2[r][c]);
+                }printf("\n");
+            }
+        break;
+        
+        case 4:
+            printf("\nSubstraction is: \n");
+            for(r=0; r<row; r++){
+                for(c=0; c<col; c++){
+                    printf("%d\t",arr1[r][c] - arr2[r][c]);
+                }printf("\n");
+            }
+        break;
+        
+        case 5:
+            if(row==col){
+                printf("\nMultiplication is: \n");
+                for(r=0; r<row; r++){
+                    for(c=0; c<col; c++){
                         mul[r][c] = 0;
                         for(int k=0; k<col; k++){
                             mul[r][c] += arr1[r][k] * arr2[k][c];
                         }
-                        printf("%d\t", mul[r][c]);
-                    }
-                    printf("\n");
+                        printf("%d\t",mul[r][c]);
+                    }printf("\n");
                 }
-                break;
+            }else{
+                printf("\n-> matrix multiplication not possible.\n");
+            }
+        break;
         
-        case 0: printf("\nExited."); exit(0);
+        case 6:
+            printf("\nTransposing array 1: \n");
+            for(c=0; c<col; c++){
+                for(r=0; r<row; r++){
+                    printf("%d\t",arr1[r][c]);
+                }printf("\n");
+            }
+            printf("\nTransposing array 2: \n");
+            for(c=0; c<col; c++){
+                for(r=0; r<row; r++){
+                    printf("%d\t",arr2[r][c]);
+                }printf("\n");
+            }
+        break;
         
-        default: printf("\n-> Enter Valid choice. \n"); break;
+        case 0: printf("\n-> Exited.\n"); exit(0);
+        
+        default: printf("\n-> enter valid choice.\n");
+            
+            
     }
     }
-    
-    return 0;
 }
