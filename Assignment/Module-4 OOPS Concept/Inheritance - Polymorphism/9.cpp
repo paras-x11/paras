eefaa8abc90f7f13bc5d1098b42d5064d1db88f9
paras <ranaparas9375@gmail.com>
2024-07-21 14:16:09 +0530

    add

---
+++ Assignment/Module-4 OOPS Concept/Inheritance - Polymorphism/9.cpp
@@ -0,0 +1,14 @@
+// 9. Write a Program of Two 1D Matrix Addition using Operator Overloading 
+
+#include <iostream>
+using namespace std;
+
+class oned{
+    
+};
+
+int main(){
+    
+
+    return 0;
+}
--- Assignment/Module-4 OOPS Concept/practice/task.cpp
+++ Assignment/Module-4 OOPS Concept/practice/task.cpp
@@ -2,11 +2,11 @@
 // 2. wa menu driven program to calculate area of circle,rectangle,triangle.
 // 3. write a program to calculate factorial using loop.
 // 4. write a prg to calculate fibonacci series.
-// 5. w a p tp check whether number is palindrome using do...while loop.
+// 5. w a p to check whether number is palindrome using do...while loop.
 // 6. write a program for jump statement.
 // 7. write a prg for function with call by value.
 // 8. w a p for function with call by reference.
-// 9. write a prg for  1d array with dynamic values.
+// 9. write a prg for 1d array with dynamic values.
 // 10. write a prg for 2d array with dynamic values.
 
 // === class ,object,constructor and destructor ===
