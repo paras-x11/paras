7e95b94472fb07972d6d3f15b8016f517952968a
paras <ranaparas9375@gmail.com>
2024-12-17 09:27:50 +0530

    add

---
+++ Assignment/Python/Practice/All/mysql_ex2.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host = "localhost",
+    database = "practice",
+    username = "root",
+    password = "2530"
+)
+
+query = conn.cursor()
+
+qry = "select * from employee"
+query.execute(qry)
+
+results = query.fetchall()
+for row in results:
+    print(row)
+
+print("success")
+conn.commit()
+conn.close()
---
+++ Assignment/Python/Practice/All/regex2.py
@@ -0,0 +1,14 @@
+import re
+
+str = "Roy is 43 Babu is 27 RoHaN is 35 SohaM is 18 balA is 29"
+
+# print(re.match(r'[A-Z][a-z]*', str))   # Checks if the pattern matches at the beginning of the string.
+# print(re.search(r'[A-Z][a-z]*', str))   # Returns a match object if found(only 1st occurence), or None if not found.
+
+# match_result = re.findall(r'[A-Z]', str)
+# search_result = re.findall(r'[A-Z][a-z]*', str)   
+
+# print(match_result)
+# print(search_result)
+
+print(re.match("^[a-z0-9]+@tops-int.com$","test@tops-int.com"))
