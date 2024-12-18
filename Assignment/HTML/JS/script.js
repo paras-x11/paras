0770c7cd66fc15c6b77d95313738f0abca9867f3
paras <ranaparas9375@gmail.com>
2024-12-18 13:58:56 +0530

    add

---
+++ Assignment/HTML/JS/script.js
@@ -0,0 +1,9 @@
+// DOM Manipulation
+
+let elem = document.getElementById('click')
+console.log(elem);
+
+let elemClass = document.getElementsByClassName('container ')
+console.log(elemClass);
+
+elemClass[0].style.background('yellow')
