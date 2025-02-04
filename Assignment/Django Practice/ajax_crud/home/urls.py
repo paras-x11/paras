2a2353a58ce3dd00366ed9b92e907c4961c05558
paras <ranaparas9375@gmail.com>
2025-02-04 12:10:48 +0530

    add

--- Assignment/Django Practice/ajax_crud/home/urls.py
+++ Assignment/Django Practice/ajax_crud/home/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from home.views import *
+from home import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', views.index, name="index"),
+    path('index', views.index, name="index"),
 ]
 
 
