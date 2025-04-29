90f4e46b840ba1deb2e331cb515fc454aa42241b
paras <ranaparas9375@gmail.com>
2025-04-29 11:57:15 +0530

    add

--- Assignment/Python/Practice/Test/3.py
+++ Assignment/Python/Practice/Test/3.py
@@ -20,12 +20,10 @@ class Employee:
         self._salary = salary
         print(f"\n-> Object '{self._name}' Created")
 
-    @property
     def get_salary(self):
         print(f"{self._name}'s salary is: {self._salary}")
         return self._salary
 
-    @get_salary.setter
     def set_new_salary(self, amt):
         self._salary = amt
         print(f"{self._name}'s new salary is set to: {self._salary}")
@@ -37,7 +35,6 @@ class Manager(Employee):
         super().__init__(name, salary)
         self._bonus = bonus
 
-    @property
     def get_salary(self):
         total_salary = self._salary + self._bonus
         print(f"{self._name}'s total salary (salary + bonus) is: {total_salary}")
@@ -45,16 +42,16 @@ class Manager(Employee):
 
 
 e = Employee('pavan', 20000)
-e.get_salary
-e.set_new_salary = 30000
-print("New Salary: ", e.get_salary)
+e.get_salary()
+e.set_new_salary(30000)
+print("New Salary: ", e.get_salary())
 
 print("=================================================================")
 
 m = Manager('paras', 50000, 5000)
-m.get_salary
-m.set_new_salary = 70000
-print("New Salary: ", m.get_salary)
+m.get_salary()
+m.set_new_salary(70000)
+print("New Salary: ", m.get_salary())
 
 
 
--- Assignment/Python/Practice/Test/4.py
+++ Assignment/Python/Practice/Test/4.py
@@ -1,11 +1,8 @@
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Question 4:
 # Create a class LogWriter that:
-
 # Takes a file path during initialization
-
 # Has a method write_log(message) that appends the message to the file with a timestamp
-
 # Has a method read_logs() that returns all log entries as a list of strings
 
 # class LogWriter:
@@ -13,9 +10,7 @@
 #     def write_log(self, message): ...
 #     def read_logs(self): ...
 
-
 # Expected Usage:
-
 # logger = LogWriter("app.log")
 # logger.write_log("User logged in")
 # logger.write_log("User performed action")
@@ -23,37 +18,53 @@
 # logs = logger.read_logs()
 # print(logs)
 
-
-# # Output:
-# # ["[2025-04-24 14:33:21] User logged in", 
-# "[2025-04-24 14:35:02] User performed action"]
+# Output:
+# ["[2025-04-24 14:33:21] User logged in", # "[2025-04-24 14:35:02] User performed action"]
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 import os
 # os.chdir("D:\\paras\\Assignment\\Python\\Practice\\Test\\Logs")
-import datetime
+import datetime as dt
+
+# timestamp = dt.datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')
+# print(timestamp)
 
 class LogWriter:
     def __init__(self, file_path):
         self._file_path = file_path 
-        open(self._file_path, 'a')
+        open(self._file_path, 'a').close()
 
     def write_log(self, message):
-        timestamp = 
+        timestamp = timestamp = dt.datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')
+        with open(self._file_path, 'a') as f:
+            f.write(f"[{timestamp} {message}],\n")
 
     def read_logs(self):
-        pass
-
-
-
+        with open(self._file_path, 'r') as f:
+            # logs = f.read()                 # this will give one big string
+            # return logs
+            return [line.strip() for line in f.readlines()]
+    
 
 logger = LogWriter("D:\\paras\\Assignment\\Python\\Practice\\Test\\Logs\\app.log")
 logger.write_log("User logged in")
-logger.write_log("User performed action")
+logger.write_log("User performed action 1")
+logger.write_log("User performed action 2")
 
 logs = logger.read_logs()
-print(logs)
+# print(logs)
+for log in logs:
+    print(log)
 
 
+today = dt.datetime.now()
+ 
+# Attributes
+print("Day: ", today.day)
+print("Month: ", today.month)
+print("Year: ", today.year)
+print("Hour: ", today.hour)
+print("Minute: ", today.minute)
+print("Second: ", today.second)
 
